9b8d99f101f81494fd901583a17f57024f1c6401
Marie Godon <mariefrance.godon@gmail.com>
2016-04-19 19:01:05 +0000

    finished callback exercise

---
+++ call-callbacks.js
@@ -0,0 +1,29 @@
+function firstChar(someString, callback) {
+    var first = someString.substring(0, 1);
+    setTimeout(function(){
+    callback(first);    
+    }, 1000);
+}
+
+
+function lastChar(someString, callback) {
+    var last = someString.substring(someString.length - 1, someString.length);
+    setTimeout(function(){
+    callback(last);  
+    }, 1000);
+}
+
+function getFirstAndLast(someString, callback) {
+    firstChar(someString, function(first){
+        lastChar(someString, function(last){
+            var firstAndLast = first.concat(last);
+            callback(firstAndLast);
+        });
+    }); 
+
+}
+
+getFirstAndLast("hello", function(firstLast) {
+    console.log(firstLast); // should output "ho"
+});
+
